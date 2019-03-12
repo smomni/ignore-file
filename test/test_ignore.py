9cc4fd550f650ab624e42c1b85783c5eac6b8f86
Simo Tumelius <simo.tumelius@gmail.com>
2019-03-12 07:55:34 +0200

    Change iterdir ignore_file argument is now ignore_file_name_or_path

--- test/test_ignore.py
+++ test/test_ignore.py
@@ -8,6 +8,7 @@ from ignore import iterdir
 def write_ignore_file(dir_path: Path, ignore_pattern_arr: List[str]=()):
     ignore_path = dir_path / '.ignore'
     ignore_path.write_text('\n'.join(ignore_pattern_arr))
+    return ignore_path
 
 
 def test_iterdir_raises_file_not_found_error_if_no_ignore_file_is_found(shared_datadir):
@@ -50,6 +51,14 @@ def test_iterdir_ignore_foo_files(shared_datadir):
 def test_iterdir_ignore_csv_files(shared_datadir):
     write_ignore_file(shared_datadir, ['*.csv'])
     path_arr = list(iterdir(shared_datadir))
+    assert 6 == len(path_arr)
+    for path in path_arr:
+        assert path.suffix != '.csv'
+
+
+def test_iterdir_ignore_file_location_can_be_specified(datadir, shared_datadir):
+    ignore_file_path = write_ignore_file(datadir, ['*.csv'])
+    path_arr = list(iterdir(shared_datadir, ignore_file_name_or_path=ignore_file_path))
     assert 6 == len(path_arr)
     for path in path_arr:
         assert path.suffix != '.csv'
